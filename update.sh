e0250aecfc9a51c0b5ff06f3cb2813a70459760a
u2takey <u2takey@gmail.com>
2018-08-09 16:05:11 +0800

    update.sh

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd notebook-io && ng build --prod --base-href "/notebook-hub/" && cd -
+rm *.js
+cp notebook-io/dist/notebook-io/* .
